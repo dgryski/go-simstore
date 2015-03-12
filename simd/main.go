85169941ed92778ddecf5c46aae791c48692bd9f
Damian Gryski <damian@gryski.com>
2015-03-12 16:21:51 +0100

    Add vptree support
    This lets us query distances >3.
    
    This patch also adds -vptree and -store flags to allow disabling one of
    the query engines.

--- simd/main.go
+++ simd/main.go
@@ -12,7 +12,9 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/DataWraith/vptree"
 	"github.com/dgryski/go-simstore"
+	"github.com/dgryski/go-simstore/simhash"
 )
 
 var Metrics = struct {
@@ -23,16 +25,30 @@ var Metrics = struct {
 	Signatures: expvar.NewInt("signatures"),
 }
 
+type vpnode struct {
+	h  uint64
+	id uint64
+}
+
+func hamming(a, b interface{}) float64 { return float64(simhash.Distance(a.(vpnode).h, b.(vpnode).h)) }
+
 func main() {
 
 	port := flag.Int("p", 8080, "port to listen on")
 	input := flag.String("f", "", "file with signatures to load")
+	useVPTree := flag.Bool("vptree", true, "load only vptree")
+	useStore := flag.Bool("store", true, "load the simstore")
 
 	flag.Parse()
 
 	log.Println("starting simd")
 
-	var store simstore.Store
+	var store *simstore.Store
+	if *useStore {
+		store = &simstore.Store{}
+	}
+
+	var vpt *vptree.VPTree
 
 	f, err := os.Open(*input)
 	if err != nil {
@@ -40,6 +56,7 @@ func main() {
 	}
 
 	scanner := bufio.NewScanner(f)
+	var items []interface{}
 	var lines int
 	for scanner.Scan() {
 
@@ -57,7 +74,12 @@ func main() {
 			continue
 		}
 
-		store.Add(sig, uint64(id))
+		if *useVPTree {
+			items = append(items, vpnode{sig, uint64(id)})
+		}
+		if *useStore {
+			store.Add(sig, uint64(id))
+		}
 		lines++
 
 		if lines%(1<<20) == 0 {
@@ -67,12 +89,58 @@ func main() {
 
 	log.Println("loaded", lines)
 	Metrics.Signatures.Set(int64(lines))
-	store.Finish()
-	log.Println("done")
+	if *useStore {
+		store.Finish()
+		log.Println("simstore done")
+		http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) { searchHandler(w, r, store) })
+	}
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) { searchHandler(w, r, &store) })
+	if *useVPTree {
+		vpt = vptree.New(hamming, items)
+		log.Println("vptree done")
+		http.HandleFunc("/topk", func(w http.ResponseWriter, r *http.Request) { topkHandler(w, r, vpt) })
+	}
+
 	log.Println("listening on port", *port)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
+}
+
+func topkHandler(w http.ResponseWriter, r *http.Request, vpt *vptree.VPTree) {
+
+	Metrics.Requests.Add(1)
+
+	sigstr := r.FormValue("sig")
+	sig64, err := strconv.ParseUint(sigstr, 16, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	kstr := r.FormValue("k")
+	if kstr == "" {
+		kstr = "10"
+	}
+
+	k, err := strconv.Atoi(kstr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	matches, distances := vpt.Search(vpnode{sig64, 0}, k)
+
+	type hit struct {
+		ID uint64  `json:"id"`
+		D  float64 `json:"d"`
+	}
+
+	var results []hit
+
+	for i, m := range matches {
+		results = append(results, hit{ID: m.(vpnode).id, D: distances[i]})
+	}
+
+	json.NewEncoder(w).Encode(results)
 }
 
 func searchHandler(w http.ResponseWriter, r *http.Request, store *simstore.Store) {
