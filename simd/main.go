073e4257acd4e45e9bf4b45270be1b655151b400
Damian Gryski <damian@gryski.com>
2015-09-24 16:35:21 +0200

    simd: oops, fudge factor should be % of lines, not of machines :/

--- simd/main.go
+++ simd/main.go
@@ -149,7 +149,7 @@ func loadConfig(input string, useStore bool, storeSize int, small bool, useVPTre
 
 	if totalMachines != 1 {
 		// estimate how many signatures will land on this machine, plus a fudge
-		sigsEstimate = int(uint(totalLines)/totalMachines) + int(float64(totalMachines)*0.10)
+		sigsEstimate = totalLines/totalMachines + int(float64(totalLines)*0.05)
 	}
 
 	log.Printf("preallocating for %d estimated signatures\n", sigsEstimate)
